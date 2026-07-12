8d3489119385c0f6f5b8fdde5c4043dd1e044e83
root <root@portfolio-gateway.lan>
2026-07-12 20:11:52 +0000

    fix: usar charset utf8mb4 na ligação PDO para corrigir acentuação PT-PT

--- utils/DBWrapper.php
+++ utils/DBWrapper.php
@@ -9,7 +9,7 @@ class DBWrapper {
     public function __construct() {
         global $host, $user, $pwd, $db;
         try {
-            $this->DBH = new PDO("mysql:host={$host};dbname={$db};charset=utf8", $user, $pwd);
+            $this->DBH = new PDO("mysql:host={$host};dbname={$db};charset=utf8mb4", $user, $pwd);
             $this->DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $pdoe) {
             echo "Erro na conexão: " . $pdoe->getMessage();
